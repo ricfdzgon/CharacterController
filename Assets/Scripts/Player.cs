4de79675f256a53cba4067c7eb0cda1129999ddd
Ricardo Fernández <ricfdzgon@gmail.com>
2022-03-25 18:40:29 +0100

    Girar la dirección con el ratón

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,22 @@ public class Player : MonoBehaviour
 {
     public float forwardSpeed = 5f;
     public float jumpHeight = 1.2f;
+    public float mouseRotationSensitivity = 10;
+    private float rotationSpeed = 200f;
     private Vector3 playerVelocity;
     private PlayerState state;
     private CharacterController charController;
     public Animator animator;
+    public Transform cameraMount;
     void Start()
     {
         charController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
         SetState(PlayerState.Iddle);
+        if (cameraMount == null)
+        {
+            Debug.Log("Player.Start CameraMount no inicializado");
+        }
     }
 
     void Update()
@@ -22,6 +29,10 @@ public class Player : MonoBehaviour
         //Gestión de Inputs
 
         Vector3 movementInput = Input.GetAxisRaw("Vertical") * Vector3.forward;
+        Vector2 mouseInput = Vector2.zero;
+        mouseInput.x = Input.GetAxisRaw("Mouse X");
+        mouseInput.y = -Input.GetAxisRaw("Mouse Y");
+
         movementInput = transform.TransformDirection(movementInput);
 
         //Gestión del salto
@@ -39,6 +50,22 @@ public class Player : MonoBehaviour
                 SetState(PlayerState.Fall);
             }
         }
+        //Gestión de rotación
+        //Rotación del personaje en el plano horizontal
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * mouseInput.x * mouseRotationSensitivity);
+
+        //Rotación de la cámara en el plano vertical
+        float cameraVerticalRotation = cameraMount.localEulerAngles.x;
+        cameraVerticalRotation += rotationSpeed * Time.deltaTime * mouseInput.y * mouseRotationSensitivity;
+        if (cameraVerticalRotation > 180)
+        {
+            cameraVerticalRotation -= 360;
+        }
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -20, 90);
+        cameraMount.localEulerAngles = Vector3.right * cameraVerticalRotation;
+
+        //cameraMount.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * mouseInput.y * mouseRotationSensitivity);
+
         //Gestión de movimiento
         playerVelocity.x = movementInput.x * forwardSpeed;
         playerVelocity.z = movementInput.z * forwardSpeed;
@@ -60,8 +87,6 @@ public class Player : MonoBehaviour
                 SetState(PlayerState.Iddle);
             }
         }
-
-
     }
     void SetState(PlayerState newState)
     {
