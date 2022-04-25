b9dd2381cd5fc8263b6f21d09e05a6b89aeda23f
Ricardo Fernández <ricfdzgon@gmail.com>
2022-04-25 19:18:32 +0200

    Inicio para guardar inercia plataforma

---
+++ Assets/Scripts/Speedometer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speedometer : MonoBehaviour
+{
+    private Vector3 previousPosition;
+    void Start()
+    {
+        previousPosition = transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+}
