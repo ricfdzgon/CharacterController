958ff91ddf6edb61464ff04831776a9f02bb3d65
Ricardo Fernández <ricfdzgon@gmail.com>
2022-05-02 19:04:41 +0200

    Hacer transparentes objetos que estorban en la vista

--- Assets/Scripts/CameraHorizontalMovement.cs
+++ Assets/Scripts/CameraHorizontalMovement.cs
@@ -9,7 +9,9 @@ public class CameraHorizontalMovement : MonoBehaviour
     public float mouseScrollSensitivity = 1000f;
     public Transform camerHorizontalFront;
     public Transform cameraHorizontalBack;
+    public Transform playerHead;
     private Vector3 cameraTargetPosition;
+    private RaycastHit[] hits;
     void Start()
     {
         if (camerHorizontalFront == null)
@@ -45,6 +47,49 @@ public class CameraHorizontalMovement : MonoBehaviour
                 cameraTargetPosition = cameraHorizontalBack.localPosition;
             }
         }
+
+        //Antes de buscar nuevos obstáculos,restauro los que haya puesto transparente en el frame anterior
+        if (hits != null)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.gameObject.CompareTag("Obstacle"))
+                {
+                    //Transparentamos el obstáculo
+                    Color color = hit.collider.GetComponent<Renderer>().material.color;
+                    color.a = 1f;
+                    hit.collider.GetComponent<Renderer>().material.color = color;
+                }
+            }
+        }
+
+        //La dirección del raycast es la dirección forward de la montura de la cámara
+        Vector3 rayCastDirection = (playerHead.position - transform.position);
+        float rayCastDistance = rayCastDirection.magnitude;
+        rayCastDirection = rayCastDirection.normalized;
+        hits = Physics.RaycastAll(transform.position, rayCastDirection, rayCastDistance);
+
+        Debug.Log("CameraHorizontalMovement.update raycastDirection " + rayCastDirection + " raycastDistance " + rayCastDistance + " hits " + hits.Length);
+
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.gameObject.CompareTag("Obstacle"))
+            {
+                //Transparentamos el obstáculo
+                Color color = hit.collider.GetComponent<Renderer>().material.color;
+                color.a = 0.2f;
+                hit.collider.GetComponent<Renderer>().material.color = color;
+            }
+            else
+            {
+                if (hit.collider.gameObject.CompareTag("Wall"))
+                {
+                    //Acercamos la cámara a Amy lo máximo posible
+
+                }
+            }
+        }
+
         transform.localPosition = Vector3.Lerp(transform.localPosition, cameraTargetPosition, cameraLerpSpeed * Time.deltaTime);
     }
 }
