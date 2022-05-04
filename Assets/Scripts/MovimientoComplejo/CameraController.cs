4c60a7c95ecfff10a7a99c0ff168b4d85db539ca
Ricardo Fernández <ricfdzgon@gmail.com>
2022-05-04 19:58:03 +0200

    Scroll de la cámara

--- Assets/Scripts/MovimientoComplejo/CameraController.cs
+++ Assets/Scripts/MovimientoComplejo/CameraController.cs
@@ -10,13 +10,27 @@ public class CameraController : MonoBehaviour
     [Header("Movement")]
     public Transform target;
 
+    [Header("Scroll")]
+    public float scrollSensitivity = 100;
+    public float scrollSpeed = 5;
+    public float minDistance = 1, maxDistance = 6, defaultDistance = 3;
+    private float currentScrollDistance;
+    private Transform cameraTransform;
+
     void Start()
     {
-
+        currentScrollDistance = defaultDistance;
+        cameraTransform = GetComponentInChildren<Camera>().transform;
     }
 
     void Update()
     {
         transform.position = target.position + pivot;
+
+        //Movemos la cámara adelante y atrás con el scroll del ratón
+        currentScrollDistance -= Input.mouseScrollDelta.y * scrollSensitivity * scrollSpeed * Time.deltaTime;
+        currentScrollDistance = Mathf.Clamp(currentScrollDistance, minDistance, maxDistance);
+        Vector3 cameraPosition = new Vector3(0, 0, -currentScrollDistance);
+        cameraTransform.localPosition = cameraPosition;
     }
 }
