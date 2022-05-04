21b3161084989f199c8391f71bc4d30bde05d094
Ricardo Fernández <ricfdzgon@gmail.com>
2022-05-04 20:52:23 +0200

    Inicio movimiento Amy dirección de la cámara

--- Assets/Scripts/MovimientoComplejo/AmyMovement.cs
+++ Assets/Scripts/MovimientoComplejo/AmyMovement.cs
@@ -6,18 +6,32 @@ public class AmyMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float speed = 5f;
-
+    private Vector3 playerVelocity = Vector3.zero;
     private CharacterController charController;
     void Start()
     {
         charController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
+        
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            charController.Move(transform.forward * speed * Time.deltaTime);
-        }
+        //Leer las entradas del jugador
+        Vector3 movementInput = Vector3.zero;
+        movementInput.x = Input.GetAxis("Horizontal");
+        movementInput.z = Input.GetAxis("Vertical");
+        movementInput = movementInput.normalized;
+
+        movementInput = Camera.main.transform.TransformDirection(movementInput);
+
+        movementInput.y = 0;
+
+        playerVelocity = movementInput * speed;
+        playerVelocity.y += Physics.gravity.y * Time.deltaTime;
+
+        charController.Move(playerVelocity * Time.deltaTime);
     }
 }
