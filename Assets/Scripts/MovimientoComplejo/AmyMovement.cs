ff1dc62d9ca76f45dd33b002399c4c406b716616
Ricardo Fernández <ricfdzgon@gmail.com>
2022-05-06 19:16:00 +0200

    Orientar a Amy en la dirección de la cámara
    El Speedometer sirve para ver la velocidad de un GameObject y su Vector3

--- Assets/Scripts/MovimientoComplejo/AmyMovement.cs
+++ Assets/Scripts/MovimientoComplejo/AmyMovement.cs
@@ -8,13 +8,14 @@ public class AmyMovement : MonoBehaviour
     public float speed = 5f;
     private Vector3 playerVelocity = Vector3.zero;
     private CharacterController charController;
+    public float rotationLerpSpeed = 20f;
     void Start()
     {
         charController = GetComponent<CharacterController>();
 
+        //Esto es para en modo Play que no se escape el ratón por ahí
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.lockState = CursorLockMode.Confined;
-        
     }
 
     void Update()
@@ -23,15 +24,25 @@ public class AmyMovement : MonoBehaviour
         Vector3 movementInput = Vector3.zero;
         movementInput.x = Input.GetAxis("Horizontal");
         movementInput.z = Input.GetAxis("Vertical");
-        movementInput = movementInput.normalized;
 
         movementInput = Camera.main.transform.TransformDirection(movementInput);
 
         movementInput.y = 0;
+        movementInput = movementInput.normalized;
 
-        playerVelocity = movementInput * speed;
+        playerVelocity.x = movementInput.x * speed;
+        playerVelocity.z = movementInput.z * speed;
+
+        if (charController.isGrounded)
+        {
+            playerVelocity.y = 0;
+        }
+
         playerVelocity.y += Physics.gravity.y * Time.deltaTime;
 
         charController.Move(playerVelocity * Time.deltaTime);
+
+        //Orientamos a Amy en la dirección de movimiento
+        transform.forward = Vector3.Lerp(transform.forward, movementInput, rotationLerpSpeed * Time.deltaTime);
     }
 }
