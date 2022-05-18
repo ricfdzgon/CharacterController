ff2a3461a418da7efbb4477efc7d61f6ee178646
Ricardo Fernández <ricfdzgon@gmail.com>
2022-05-18 20:39:08 +0200

    Script para activar o desactivar el ragdoll y animator

---
+++ Assets/Scripts/MovimientoComplejo/RagdollOnOff.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollOnOff : MonoBehaviour
+{
+    private Animator animator;
+
+    private bool ragdollActivated;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetActivated(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SetActivated(!ragdollActivated);
+        }
+    }
+
+    void SetActivated(bool active)
+    {
+        animator.enabled = !active;
+
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.isKinematic = !active;
+        }
+
+        ragdollActivated = active;
+    }
+}
